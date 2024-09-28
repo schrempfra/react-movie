95637fd031c9ecf8efc8d8e443248a083d9cd608
Raphael Schrempf <schrempfra@gmail.com>
2024-09-28 22:32:04 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,9 @@ export default function Home() {
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`)
       .then(response => response.json())
       .then(data => {
-        setTimeout(() => {
-          setMovies(data.results); // Assuming the API returns an array of movies in `data.results`
-          setLoading(false); // Stop loading
-        }, 1500);
+        console.log(data);
+        setMovies(data.results); // Assuming the API returns an array of movies in `data.results`
+        setLoading(false); // Stop loading
       })
       .catch(error => {
         setError(error);
@@ -35,35 +34,66 @@ export default function Home() {
         <div className="text-red-500 text-center">An error occurred: {error.message}</div>
       ) : loading ? ( // Show loading indicator
         <div className="flex justify-center items-center h-64">
-          <div className="loader"></div>
+          <div className="loader"></div> {/* Spinner */}
         </div>
       ) : (
         <>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {movies.map(movie => (
-              <div key={movie.id} className="bg-white shadow-md rounded-xl overflow-hidden transform transition-transform hover:scale-105 hover:shadow-xl">
-                <Image
-                  className="w-full h-60 object-cover"
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                  width={150}
-                  height={225}
-                  priority
-                />
-                <div className="p-3">
-                  <h2 className="text-2xl font-extrabold text-gray-900 mb-2">{movie.title}</h2>
-                  <p className="text-gray-700 mb-2"><strong className="font-semibold">Release Date:</strong> {movie.release_date}</p>
-                  <p className="text-gray-700 mb-2"><strong className="font-semibold">Runtime:</strong> {movie.runtime} minutes</p>
+              <div key={movie.id} className="overflow-hidden">
+                <div className="relative w-full h-80 bg-gray-800 rounded-md">
+                  <Image
+                    className="w-full h-80 object-cover rounded-md "
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    width={150}
+                    height={225}
+                    priority
+                  />
+                  <div className="absolute -bottom-5 right-2 w-10 h-10 bg-gray-200 rounded-full dark:bg-gray-700 flex items-center justify-center">
+                    <div className="relative w-full h-full rounded-full flex items-center justify-center text-white text-xs">
+                      <svg className="absolute top-0 left-0 w-full h-full" viewBox="0 0 36 36">
+                        <path
+                          className="text-blue-500"
+                          strokeDasharray={`${Math.round(movie.vote_average * 10)}, 100`}
+                          d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                        />
+                      </svg>
+                      {Math.round(movie.vote_average * 10)}%
+                    </div>
+                  </div>
+                </div>
+
+                <div className="p-3 text-white">
+                  <h2 className="text-md font-extrabold">{movie.title}</h2>
+                  <p className="mb-2 text-md">Release Date: {new Date(movie.release_date).toLocaleDateString('de-DE')}</p>
                 </div>
               </div>
             ))}
           </div>
           <div className="flex justify-between mt-4">
-            <button onClick={handlePrevPage} disabled={page === 1} className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50">Previous</button>
-            <button onClick={handleNextPage} className="bg-blue-500 text-white px-4 py-2 rounded">Next</button>
+            <button
+              onClick={handlePrevPage}
+              disabled={page === 1}
+              className="bg-gradient-to-r from-blue-500 to-blue-700 text-white px-6 py-3 rounded-full shadow-lg transform transition-transform duration-300 hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNextPage}
+              className="bg-gradient-to-r from-blue-500 to-blue-700 text-white px-6 py-3 rounded-full shadow-lg transform transition-transform duration-300 hover:scale-105"
+            >
+              Next
+            </button>
           </div>
         </>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 }
