b4f8c5b4a14b55bfc9068e8b0db4530a624260bf
Raphael Schrempf <schrempfra@gmail.com>
2024-09-29 11:06:04 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         fetch(url)
             .then(response => response.json())
             .then((data: ApiResponse) => {
-                setMovies(data.results);
+                setMovies(data.results || []); // Ensure movies is always an array
                 setTotalPages(data.total_pages);
                 setLoading(false);
             })
@@ -106,49 +106,53 @@ export default function Home() {
             ) : (
                 <>
                     <div></div>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-4">
-                        {movies.map(movie => (
-                            <div key={movie.id} className="overflow-hidden">
-                                <div className="relative w-full h-80 bg-gray-800 rounded-md">
-                                    {movie.poster_path ? (
-                                        <Image
-                                            className="w-full h-80 object-cover rounded-md"
-                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                            alt={movie.title}
-                                            width={150}
-                                            height={225}
-                                            priority
-                                        />
-                                    ) : (
-                                        <div className="w-full h-80 flex items-center justify-center bg-gray-700 text-white">
-                                            No Image Available
-                                        </div>
-                                    )}
-                                    <div className="absolute -bottom-5 right-2 w-10 h-10 bg-gray-200 rounded-full dark:bg-gray-700 flex items-center justify-center">
-                                        <div className="relative w-full h-full rounded-full flex items-center justify-center text-white text-xs">
-                                            <svg className="absolute top-0 left-0 w-full h-full" viewBox="0 0 36 36">
-                                                <path
-                                                    className="text-blue-500"
-                                                    strokeDasharray={`${Math.round(movie.vote_average * 10)}, 100`}
-                                                    d="M18 2.0845
-                            a 15.9155 15.9155 0 0 1 0 31.831
-                            a 15.9155 15.9155 0 0 1 0 -31.831"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    fill="none"
-                                                />
-                                            </svg>
-                                            {Math.round(movie.vote_average * 10)}%
+                    {movies.length > 0 ? (
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-4">
+                            {movies.map(movie => (
+                                <div key={movie.id} className="overflow-hidden">
+                                    <div className="relative w-full h-80 bg-gray-800 rounded-md">
+                                        {movie.poster_path ? (
+                                            <Image
+                                                className="w-full h-80 object-cover rounded-md"
+                                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                alt={movie.title}
+                                                width={150}
+                                                height={225}
+                                                priority
+                                            />
+                                        ) : (
+                                            <div className="w-full h-80 flex items-center justify-center bg-gray-700 text-white">
+                                                No Image Available
+                                            </div>
+                                        )}
+                                        <div className="absolute -bottom-5 right-2 w-10 h-10 bg-gray-200 rounded-full dark:bg-gray-700 flex items-center justify-center">
+                                            <div className="relative w-full h-full rounded-full flex items-center justify-center text-white text-xs">
+                                                <svg className="absolute top-0 left-0 w-full h-full" viewBox="0 0 36 36">
+                                                    <path
+                                                        className="text-blue-500"
+                                                        strokeDasharray={`${Math.round(movie.vote_average * 10)}, 100`}
+                                                        d="M18 2.0845
+                                a 15.9155 15.9155 0 0 1 0 31.831
+                                a 15.9155 15.9155 0 0 1 0 -31.831"
+                                                        stroke="currentColor"
+                                                        strokeWidth="2"
+                                                        fill="none"
+                                                    />
+                                                </svg>
+                                                {Math.round(movie.vote_average * 10)}%
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className="p-3 text-white">
+                                        <h2 className="text-md font-extrabold">{movie.title}</h2>
+                                        <p className="mb-2 text-md">{new Date(movie.release_date).toLocaleDateString('de-DE')}</p>
+                                    </div>
                                 </div>
-                                <div className="p-3 text-white">
-                                    <h2 className="text-md font-extrabold">{movie.title}</h2>
-                                    <p className="mb-2 text-md">{new Date(movie.release_date).toLocaleDateString('de-DE')}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="text-center text-white">No movies found.</div>
+                    )}
                     <div className="flex justify-between mt-4">
                         <button
                             onClick={handlePrevPage}
